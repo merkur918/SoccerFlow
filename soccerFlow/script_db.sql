2be6e63ff0a298e2f8f050b3e32f850892b020a7
MarcosH <marc.hurt.ado2812@gmail.com>
2026-01-21 16:23:48 +0100

    Pequeño cambio en la tabla email_verifications

--- soccerFlow/script_db.sql
+++ soccerFlow/script_db.sql
@@ -12,6 +12,7 @@ CREATE TABLE email_verifications (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     verified_at TIMESTAMP NOT NULL,
+    token VARCHAR(255) NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(ID) ON DELETE CASCADE
 );
