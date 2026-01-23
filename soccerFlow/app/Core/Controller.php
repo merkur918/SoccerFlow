7f36c3afbb070241078acb8a35fae52657f9ac6f
david <davgarcol05@gmail.com>
2026-01-23 15:17:17 +0100

    Cambios en el css

--- soccerFlow/app/Core/Controller.php
+++ soccerFlow/app/Core/Controller.php
@@ -7,7 +7,10 @@ class Controller {
     protected function render(string $view, array $data = []){
         extract($data);
 
+         $viewPath = $view; 
          $cssFile = str_replace('/', '-', $view) . '.css';
+         $jsFile = str_replace('/', '-', $view) . '.js';
+         
 
         require_once __DIR__ . '/../Views/layouts/main.php';
 
--- soccerFlow/app/Views/layouts/main.php
+++ soccerFlow/app/Views/layouts/main.php
@@ -7,11 +7,19 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jersey+10&display=swap" rel="stylesheet">
+     <link rel="shortcut icon" href="/assets/img/logo.png" />
+   
 
 
       <!-- CSS específico de la vista -->
     <?php if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/assets/css/$cssFile")): ?>
         <link rel="stylesheet" href="/assets/css/<?= $cssFile ?>">
+    <?php endif; ?>
+
+    <!-- JS específico de la vista -->
+
+      <?php if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/assets/js/$jsFile")): ?>
+       <script src="/assets/js/<?= $jsFile ?>"></script>
     <?php endif; ?>
 </head>
 <body>
