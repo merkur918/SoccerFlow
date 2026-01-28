62e50ef0772a59dac5f3ba5aa3d47e29b53d2c7c
MarcosH <marc.hurt.ado2812@gmail.com>
2026-01-28 17:36:44 +0100

    Creacion de vistas y estilos para la verificación de correo electrónico

---
+++ soccerFlow/app/Views/auth/Email-Ver.php
@@ -0,0 +1,19 @@
+<div class="Email__body">
+
+
+    <div class="Email__body">
+
+
+        <div class="Email__header">
+            <h1 class="Email_title">SOCCER FLO<span style="color: #079C40;">W</span></h1>
+            <img class="Email__logo" src="/assets/img/logo.png" alt="Logo Soccer Flow">
+
+        </div>
+        <div class="Email__conf">
+
+            <h2>CORREO ELECTRÓNICO VERIFICADO</h2>
+
+            <p class="Email__notice">¡Hemos verificado tu dirección de correo electrónico!<br><span style="color: #079C40;"><a>Haz click aqui para logear</a></span></p>
+
+    </div>
+</div>
---
+++ soccerFlow/app/Views/auth/Email.php
@@ -0,0 +1,15 @@
+    <div class="email">
+    <div class="email__body">
+        <div class="email__header">
+            <h1 class="email__title">SOCCER FLO<span style="color: #079C40;">W</span></h1>
+            <img class="email__logo" src="logo.png" alt="Logo Soccer Flow">
+        </div>
+        <div class="email__form">
+            <h2>CONFIRMACIÓN DE CORREO ELECTRÓNICO</h2>
+            
+            <p class="email__notice">Hemos enviado un correo de verificación a tu dirección de correo electrónico.<br><span style="color: #079C40;">¡Por favor, revisa tu bandeja de entrada y sigue las instrucciones para completar el proceso de verificación!</span></p>
+            
+         
+        </div>
+    </div>
+</div>
