6f773b916b868e4cd8442fb1010191e32af69784
MarcosH <marc.hurt.ado2812@gmail.com>
2026-02-04 17:44:51 +0100

    Barra de busqueda con filtros añadido a la vista de productos.

--- soccerFlow/app/Views/products/vistaProductos.php
+++ soccerFlow/app/Views/products/vistaProductos.php
@@ -1,39 +1,110 @@
-  <div class="product__body-window">
-    <div class="product__block">
-        <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
-        <h3>Nombre del Producto</h3>
-        <p class="precio">Precio: $XX.XX</p>
-       <p><span class="product__size-hidden">tallas:</span></p>
-    </div>
-    <div class="product__block">
-        <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
-        <h3>Nombre del Producto</h3>
-        <p class="precio">Precio: $XX.XX</p>
-       <p><span class="product__size-hidden">tallas:</span></p>
-    </div>
+<div class="product__body-window">
+        <div class="product__search">
+            <div ></div>
+             <input type="text" placeholder="Buscar productos..." class="product__search-input">
+            <div class="product__order-filter">
 
-    <div class="product__block">
-        <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
-        <h3>Nombre del Producto</h3>
-        <p class="precio">Precio: $XX.XX</p>
-       <p><span class="product__size-hidden">tallas:</span></p>
-    </div>
-    <div class="product__block">
-        <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
-        <h3>Nombre del Producto</h3>
-        <p class="precio">Precio: $XX.XX</p>
-       <p><span class="product__size-hidden">tallas:</span></p>
-    </div>
-    <div class="product__block">
-        <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
-        <h3>Nombre del Producto</h3>
-        <p class="precio">Precio: $XX.XX</p>
-       <p><span class="product__size-hidden">tallas:</span></p>
-    </div>
-    <div class="product__block">
-        <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
-        <h3>Nombre del Producto</h3>
-        <p class="precio">Precio: $XX.XX</p>
-       <p><span class="product__size-hidden">tallas:</span></p>
+               <select class="product__order-price">
+                   <option value="default">Ordenar por</option>
+                   <option value="price-asc">Precio: Bajo a Alto</option>
+                   <option value="price-desc">Precio: Alto a Bajo</option>
+                </select>
+
+                 <select class="product__filter-category">
+                    <option value="all">Filtrar por categoría</option>
+                    <option value="shoes">Botas</option>
+                    <option value="t-shirts">Camisetas</option>
+                    <option value="gloves">Guantes</option>
+                    <option value="pants">Chandals</option>
+                </select>
+
+                <select class="product__filter-size">
+                    <option value="all-sizes">Filtrar por talla</option>
+                    <option value="size-38">Talla 38</option>
+                    <option value="size-39">Talla 39</option>
+                    <option value="size-40">Talla 40</option>
+                    <option value="size-41">Talla 41</option>
+                    <option value="size-42">Talla 42</option>
+                    <option value="size-43">Talla 43</option>
+                </select>
+
+                <select class="product__filter-teams">
+                    <option value="all-teams">Filtrar por equipo</option>
+                    <option value="team-a">Equipo A</option>
+                    <option value="team-b">Equipo B</option>
+                    <option value="team-c">Equipo C</option>
+                </select>
+
+                <select class="product__filter-brand">
+                    <option value="all-brands">Filtrar por marca</option>
+                    <option value="brand-a">Marca A</option>
+                    <option value="brand-b">Marca B</option>
+                    <option value="brand-c">Marca C</option>
+                </select>
+
+                <select class="product__filter-gender">
+                    <option value="all-genders">Filtrar por género</option>
+                    <option value="male">Hombre</option>
+                    <option value="female">Mujer</option>
+                    <option value="unisex">Unisex</option>
+                </select>
+                    
+
+            </div>
+           
+        </div>
+        
+        <!-- LOS PRODUCTOS DEBEN ESTAR DENTRO DE ESTE CONTENEDOR -->
+        <div class="product__container">
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 1</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 2</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 3</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 4</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 5</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 6</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 7</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+            <div class="product__block">
+                <img src="zapatillas.jpg" alt="imagen del producto" class="product__image-window">
+                <h3>Nombre del Producto 8</h3>
+                <p class="precio">Precio: $XX.XX</p>
+                <p><span class="product__size-hidden">tallas:</span></p>
+            </div>
+        </div>
+      
     </div>
     </div>
